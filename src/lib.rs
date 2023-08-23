57c25b26b612a306dca97e21ac020ac9788673ce
Josh Teng <tengjosh+github@gmail.com>
2023-08-24 00:02:04 +0800

    export rent constant

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 anchor_gen::generate_cpi_interface!(idl_path = "idl.json");
 
+pub const RENT_TO_INITIALIZE_ACCOUNTS: u64 = 2039280 + 2039280 + 2902320;
+
 declare_id!("DCA265Vj8a9CEuX1eb1LWRnDT7uK6q1xMipnNyatn23M");
